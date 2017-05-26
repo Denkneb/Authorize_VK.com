74f65ecb3e991d06bc9306a6a7b3ca31a81093aa
Denk <denkneb@gmail.com>
2017-05-26 20:48:17 +0700

    refactoring of application

--- auth_vk/views.py
+++ auth_vk/views.py
@@ -1,6 +1,6 @@
 import requests
 from django.shortcuts import render, redirect
-import webbrowser
+
 
 # https://vk.com/dev
 VK_CLIENT_ID = '6043918'
@@ -11,52 +11,72 @@ REDIRECT_URL = 'http://127.0.0.1:8005/authorize/'
 
 # Show page authorization
 def viewLogin(request):
-    return render(request, 'login.html')
+    result = None
+    if 'access_token_user' in request.session:
+        result = redirect('/profile/')
+    else:
+        result = render(request, 'login.html')
+    return result
 
 
 # Get code, access_token then user info and user friends info
 def authorizeVK(request):
+    result = None
     if not request.GET:
+        # try:
+        #     if not request.GET:
         # Open new tab, get code for getting access_token
-        # example answer server Vk.com: REDIRECT_URI?code=41b93f4831f1466548
+        # example answer server vk.com: REDIRECT_URI?code=41b93f4831f1466548
         # user will have to authorize in and allow the application
-        webbrowser.open('https://oauth.vk.com/authorize?'
-                        'client_id=' + VK_CLIENT_ID +
-                        '&display=mobile'
-                        '&redirect_uri=' + REDIRECT_URL +
-                        '&scope=friends'
-                        '&response_type=code'
-                        '&v=5.64',
-                        new=0, autoraise=True)
-        exit()
-    else:
+        result = redirect('https://oauth.vk.com/authorize?'
+                          'client_id=' + VK_CLIENT_ID +
+                          '&display=mobile'
+                          '&redirect_uri=' + REDIRECT_URL +
+                          '&scope=friends'
+                          '&response_type=code'
+                          '&v=5.64')
+    elif 'error' not in request.GET:
         # get code of answer server Vk.com
         code = request.GET['code']
 
-        # get access_token
+        # get access_token, user_id, expires_in
         # example answer server Vk.com:
         # {"access_token":"533bacf01e11f55b536a565b57531ac114461ae8736d6506a3",
         # "expires_in":43200, "user_id":66748}
-        get_token = requests.get('https://oauth.vk.com/access_token?'
-                                 'client_id=' + VK_CLIENT_ID +
-                                 '&client_secret=' + VK_CLIENT_SECRET +
-                                 '&redirect_uri=' + REDIRECT_URL +
-                                 '&code=' + code).json()
-        access_token = get_token['access_token']
-        user_id = str(get_token['user_id'])
+        request.session['access_token_user'] = requests.get('https://oauth.vk.com/access_token?'
+                                                            'client_id=' + VK_CLIENT_ID +
+                                                            '&client_secret=' + VK_CLIENT_SECRET +
+                                                            '&redirect_uri=' + REDIRECT_URL +
+                                                            '&code=' + code).json()
+        # Set the time of the session. Is equal to the duration of the application access_token.
+        request.session.set_expiry(request.session['access_token_user']['expires_in'])
 
+        result = redirect('/profile/')
+    else:
+        result = redirect('/')
+
+    return result
+
+
+# Show page profile
+def showVK(request):
+    result = None
+    if 'access_token_user' in request.session:
         # get user info
         user_get = requests.get('https://api.vk.com/method/users.get?'
-                                'user_ids=' + user_id +
+                                'user_ids=' + str(request.session['access_token_user']['user_id']) +
                                 '&fields=photo_200,bdate,country,city'
+                                '&lang=ru'
                                 '&v=5.64').json()
         # get info about user friends
         users_get = requests.get('https://api.vk.com/method/friends.get?'
                                  'order=random'
                                  '&count=5'
                                  '&fields=nickname,photo_50'
-                                 '&access_token=' + access_token +
+                                 '&access_token=' + request.session['access_token_user']['access_token'] +
+                                 '&lang=ru'
                                  '&v=5.62').json()
+
         user = {
             'id': user_get['response'][0]['id'],
             'first_name': user_get['response'][0]['first_name'],
@@ -67,8 +87,6 @@ def authorizeVK(request):
             'city': user_get['response'][0]['city']['title'],
             'users_count': users_get['response']['count']
         }
-        # Send user info to the session
-        request.session['user'] = user
 
         users = {}
         for i in users_get['response']['items']:
@@ -79,16 +97,18 @@ def authorizeVK(request):
                 'last_name': i['last_name'],
                 'photo_50': i['photo_50'],
             }
-            # Send users info to the session
-        request.session['users'] = users
 
-    return redirect('/profile/')
+        context = {
+            'user': user,
+            'users': users,
+        }
+        result = render(request, 'profile.html', context)
+    else:
+        result = redirect('/')
+    return result
 
 
-# Show page profile
-def showVK(request):
-    context = {
-        'user': request.session['user'],
-        'users': request.session['users']
-    }
-    return render(request, 'profile.html', context)
+# Logout user
+def userLogout(request):
+    del request.session['access_token_user']
+    return redirect('/')
--- vk_com/settings.py
+++ vk_com/settings.py
@@ -106,3 +106,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, "static"),
 )
+
+# production
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
